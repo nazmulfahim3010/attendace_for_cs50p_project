7b8dcec77622dd342505b85aa8a230918878259e
nazmulfahim3010 <nazmulfahim000@gmail.com>
2025-01-03 11:51:40 +0600

    happy new year wissh with joke

---
+++ fam.py
@@ -0,0 +1,74 @@
+import cowsay
+import pyfiglet
+import time
+
+
+cowsay.turtle("hey!!!you🫵🏻")
+time.sleep(3)
+cowsay.turtle("you know i want to tell you 👉🏻👈🏻")
+time.sleep(3)
+cowsay.turtle("you wanna listen ?! 🥹")
+time.sleep(3)
+cowsay.turtle("wait let me get ready😩Just a moment..😉")
+time.sleep(4)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+print("wait.....",end="")
+for _ in range(15):
+    term="❤️"
+    print(f"  {term}",end="")
+    time.sleep(0.1)
+time.sleep(1)
+print("❤️",end="")
+time.sleep(2)
+print("..❤️❤️",end="")
+time.sleep(2)
+print("...❤️❤️❤️❤️",end="")
+time.sleep(2)
+print("....❤️❤️❤️❤️❤️",end="")
+time.sleep(2)
+print(".........❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️")
+time.sleep(1)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+
+time.sleep(0)
+cowsay.kitty("ekta kocchop re bissah korle emn wait kora lage 🤡")
+time.sleep(4)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+time.sleep(0)
+
+print("TURTLE  ->  almost done arektu somoy 🥹\n\n\n\n please🥹",end="")
+for _ in range(20):
+    term="🥹"
+    print(f"  please.  {term}",end="")
+    
+
+    time.sleep(0.45)
+
+
+
+
+
+print("\n\n\n\n\n\n")
+time.sleep(0)
+
+cowsay.stegosaurus("taaaaaa..dddaaaa🥳✨")
+
+time.sleep(4)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+
+# Add a 2-second delay
+time.sleep(0)
+
+# Create a pyfiglet object with the specified font and justification
+p = pyfiglet.Figlet(font="slant", justify="center")
+
+# Print the "HAPPY NEW YEAR" message in the Figlet font
+print(p.renderText("HAPPY NEW YEAR"))
+
+
+
+
+
