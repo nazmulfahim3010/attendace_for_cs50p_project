01683f64afd3ebd570f421a2152e5865dd74f6d3
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-05-16 21:17:23 +0600

    attendance count done

--- test_.py
+++ test_.py
@@ -1,10 +1,41 @@
+#give attedance
+
+import pandas as pd
+from datetime import date
 import csv
-import cowsay 
-from teacher_login_interface import verfication_of_data
-cowsay.tux("Hi, I'm Tux the Penguin!")
-cowsay.beavis("Uhhh... Hello!")
-cowsay.dragon("I am a dragon 🐉!")
-cowsay.ghostbusters("Who you gonna call?")
-cowsay.kitty("Meow!")
-cowsay.meow("Meeow!")
+
+with open("student sheet.csv","r") as file:
+    lines=csv.DictReader(file)
+    attendance=[]
+    id_s=[]
+
+    for line in lines:
+
+        attendance.append(int(line['attendance']))
+        id_s.append(line["id"])
+
+for i in range(len(attendance)):
+    is_bool=True
+    
+    while is_bool:
+        x=input(f"{id_s[i]} ")
+        if x=="p":
+            attendance[i]+=1
+            is_bool=False
+        elif x=='a':
+            attendance[i]+=0
+            is_bool=False
+        else:
+            is_bool=True
+            
+            ...    
+
+df=pd.read_csv("student sheet.csv")
+df['attendance']=attendance
+df.to_csv('student sheet.csv',index=False)
+...
+
+
+
+
 
