9dd9c14863f21b191772cd814f6ad69e892d489c
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-05-19 21:51:32 +0600

    different file open in student_data_input_sys

--- student_data_input_sys.py
+++ student_data_input_sys.py
@@ -11,13 +11,24 @@ def data_input():
         "att":[]
 
     }
+    batch_id={
+        "1st":"212-134-",
+        "2nd":"221-134-",
+        "3rd":"222-134-",
+        "4th":"231-134-",
+        "5th":"232-134-",
+        "6th":"241-134-",
+        "7th":"242-134-"
+    }
     student_number=int(input("how many students you want to enter: "))
-    student_id="242-234-"#user input
+    student_batch=input("Your student batch number? ")
+    student_id=batch_id[student_batch]
+    sheet_name=f"{student_id} {student_batch} sheet.csv"
     for s in range(student_number+1):
         
         data["id"].append(f"{student_id}{s:03}")
         data["att"].append(0)
-        write_id_in_csv(**data)
+        write_id_in_csv(**data,filename=sheet_name)
 
     print(cowsay_msg_tux("student file has created"))
     
@@ -25,9 +36,10 @@ def data_input():
         
     ...
 def write_id_in_csv(**data):
-    
+    filename = data.get("filename", "student sheet.csv")
 
-    with open("student sheet.csv",'w',newline='') as file:
+
+    with open(filename,'w',newline='') as file:
         header=['id','attendance']
 
         sheet=csv.DictWriter(file,fieldnames=header)
